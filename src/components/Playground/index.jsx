6e68d87f0c0b8134037b44a962a24c459d9997a3
Prem Kumar <prem71717@gmail.com>
2025-03-19 14:27:12 +0530

    added info toast

--- src/components/Playground/index.jsx
+++ src/components/Playground/index.jsx
@@ -4,9 +4,31 @@ import MessageCard from "../Messages";
 
 import "./index.css";
 import { useTheme } from "../../theme/ThemeContext";
+import { ToastContainer, toast, Bounce } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { useEffect } from "react";
 
 const Playground = () => {
   const { darkMode } = useTheme();
+  const notifyApiDelay = () =>
+    toast.info(
+      "The server is starting up and will be ready in approximately 40-50 seconds. Please wait!",
+      {
+        position: "top-right",
+        autoClose: 5000,
+        hideProgressBar: true,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: false,
+        progress: undefined,
+        theme: "colored",
+        transition: Bounce,
+        backgroundColor: "red",
+      }
+    );
+  useEffect(() => {
+    notifyApiDelay();
+  }, []);
   return (
     <>
       <Box pb="2rem" justifyContent="center">
@@ -19,6 +41,7 @@ const Playground = () => {
       >
         <InputField />
       </Box>
+      <ToastContainer />
     </>
   );
 };
